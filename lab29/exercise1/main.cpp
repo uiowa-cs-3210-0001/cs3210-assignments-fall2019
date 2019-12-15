81238a0fa9e412261e823e828f419e06c3e9fd26
Aleksey Gurtovoy <agurtovoy@acm.org>
2019-12-15 16:01:59 -0600

    lab29/exercise1

---
+++ lab29/exercise1/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+int main() {
+    std::vector<std::string> names = { "Bob", "Jane", "Alice", "Bob", "Mike", "Boris" };
+
+    // TODO: sort the vector, erase all "Bob" occurrences
+    // and print the vector to the standard output using 
+    // only standard algorithms
+}
